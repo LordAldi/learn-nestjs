08e419bab788a13b70d62bcde6e1b8f043a11ff9
LordAldi <aldianugra09@gmail.com>
2021-09-15 01:03:24 +0700

    add validation

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,13 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors();
+
+  app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
   const config = new DocumentBuilder()
     .setTitle('Cats example')
